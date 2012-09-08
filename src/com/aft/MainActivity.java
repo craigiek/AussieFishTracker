4a90e7e60bffaea8995348a078eb1fdafafccb7d
Karen Davies <karen.davies@stocksoftware.com.au>
2012-09-08 18:24:08 +1000

    tidyup

--- src/com/aft/MainActivity.java
+++ src/com/aft/MainActivity.java
@@ -1,14 +1,9 @@
 package com.aft;
 
 import android.app.Activity;
-import android.content.res.Resources;
-import android.content.res.XmlResourceParser;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.LinearLayout;
-import com.aft.R.color;
 import com.aft.R.id;
-import com.aft.R.layout;
 import org.achartengine.GraphicalView;
 
 public class MainActivity
@@ -25,7 +20,7 @@ public class MainActivity
     //final Resources resources = this.getResources();
     //final String[] locations = resources.getStringArray( array.locations );
     final GraphicalView graph = new LineGraph().getView( this );
-    final LinearLayout layout = (LinearLayout)findViewById( id.chart );
+    final LinearLayout layout = (LinearLayout) findViewById( id.chart );
     layout.addView( graph );
   }
 }
