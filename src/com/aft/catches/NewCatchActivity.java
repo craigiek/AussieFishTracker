a63ad314034e0a525336106704a3a039c8fc59d8
Karen Davies <karen.davies@stocksoftware.com.au>
2012-10-15 23:01:57 +1100

    added ability to select photo for new catch from camera or gallery

--- src/com/aft/catches/NewCatchActivity.java
+++ src/com/aft/catches/NewCatchActivity.java
@@ -1,20 +1,28 @@
 package com.aft.catches;
 
+import android.R.color;
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
+import android.os.Parcelable;
+import android.provider.MediaStore;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.Toast;
-import com.aft.R;
 import com.aft.R.array;
 import com.aft.R.id;
 import com.aft.R.layout;
+import com.aft.util.ImageHelper;
+import java.util.ArrayList;
+import java.util.List;
 
 public class NewCatchActivity
   extends Activity
@@ -22,10 +30,6 @@ public class NewCatchActivity
 {
   //variable for selection intent
   private final int PICKER = 1;
-  //variable to store the currently selected image
-  private int currentPic = 0;
-  //image view for larger display
-  private ImageView _picView;
 
   private AutoCompleteTextView _species;
   private EditText _weight;
@@ -39,14 +43,14 @@ public class NewCatchActivity
     setContentView( layout.add_new_catch );
     //get the large image view
     //_picView = (ImageView) findViewById(R.id.picture);
-    _species = (AutoCompleteTextView)findViewById( id.new_catch_species );
-    _weight = (EditText)findViewById( id.new_catch_weight );
-    _location = (EditText)findViewById( id.new_catch_location );
-    _addPhotoImage = (ImageView)findViewById( id.picture);
+    _species = (AutoCompleteTextView) findViewById( id.new_catch_species );
+    _weight = (EditText) findViewById( id.new_catch_weight );
+    _location = (EditText) findViewById( id.new_catch_location );
+    _addPhotoImage = (ImageView) findViewById( id.picture );
     _addPhotoImage.setOnClickListener( this );
 
-    final String[] species = getResources().getStringArray( array.species_list);
-    ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, species);
+    final String[] species = getResources().getStringArray( array.species_list );
+    ArrayAdapter<String> adapter = new ArrayAdapter<String>( this, android.R.layout.simple_list_item_1, species );
     _species.setAdapter( adapter );
   }
 
@@ -54,8 +58,70 @@ public class NewCatchActivity
   {
     if ( v.getId() == id.picture )
     {
-      // todo kjd - launch 'add photo Camera/Gallery options'
-      Toast.makeText( this, "Add new photo clicked", Toast.LENGTH_SHORT );
+      // Camera.
+      final List<Intent> cameraIntents = new ArrayList<Intent>();
+      final Intent captureIntent = new Intent( android.provider.MediaStore.ACTION_IMAGE_CAPTURE );
+      final PackageManager packageManager = getPackageManager();
+      final List<ResolveInfo> listCam = packageManager.queryIntentActivities( captureIntent, 0 );
+      for ( final ResolveInfo res : listCam )
+      {
+        final String packageName = res.activityInfo.packageName;
+        final Intent intent = new Intent( captureIntent );
+        intent.setComponent( new ComponentName( res.activityInfo.packageName, res.activityInfo.name ) );
+        intent.setPackage( packageName );
+        cameraIntents.add( intent );
+      }
+
+      final Intent pickIntent = new Intent();
+      pickIntent.setType( "image/*" );
+      pickIntent.setAction( Intent.ACTION_GET_CONTENT );
+
+      // Chooser of filesystem/Gallery options.
+      final Intent chooserIntent = Intent.createChooser( pickIntent, "Select Source" );
+      // Add the camera options.
+      chooserIntent.putExtra( Intent.EXTRA_INITIAL_INTENTS, cameraIntents.toArray( new Parcelable[]{ } ) );
+
+      startActivityForResult( chooserIntent, PICKER );
+      //we will handle the returned data in onActivityResult
     }
+  }
+
+  protected void onActivityResult( final int requestCode, final int resultCode, final Intent data )
+  {
+    if ( resultCode == RESULT_OK )
+    {
+      //check if we are returning from picture selection
+      if ( requestCode == PICKER )
+      {
+        //import the image
+        //the returned picture URI
+        final Uri pickedUri = data.getData();
+        //declare the path string
+        String imgPath = "";
+        //retrieve the string using media data
+        String[] medData = { MediaStore.Images.Media.DATA };
+        //query the data
+        final Cursor picCursor = managedQuery( pickedUri, medData, null, null, null );
+        if ( picCursor != null )
+        {
+          //get the path string
+          int index = picCursor.getColumnIndexOrThrow( MediaStore.Images.Media.DATA );
+          picCursor.moveToFirst();
+          imgPath = picCursor.getString( index );
+        }
+        else
+        {
+          imgPath = pickedUri.getPath();
+        }
+
+        //if we have a new URI attempt to decode the image bitmap
+        if ( pickedUri != null )
+        {
+          ImageHelper.setImage( _addPhotoImage, pickedUri, imgPath, color.white );
+        }
+      }
+    }
+    //superclass method
+    super.onActivityResult( requestCode, resultCode, data );
   }
 }
--- src/com/aft/util/ImageHelper.java
+++ src/com/aft/util/ImageHelper.java
@@ -1,13 +1,17 @@
 package com.aft.util;
 
+import android.R.color;
 import android.graphics.Bitmap;
 import android.graphics.Bitmap.Config;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.PorterDuff.Mode;
 import android.graphics.PorterDuffXfermode;
 import android.graphics.Rect;
 import android.graphics.RectF;
+import android.net.Uri;
+import android.widget.ImageView;
 
 public class ImageHelper
 {
@@ -32,5 +36,50 @@ public class ImageHelper
     canvas.drawBitmap( bitmap, rect, rect, paint );
 
     return output;
+  }
+
+  public static void setImage( final ImageView view, final Uri uri, final String imagePath, final int background )
+  {
+    //set the width and height we want to use as maximum display
+    int targetWidth = view.getWidth();
+    int targetHeight = view.getHeight();;
+    //create bitmap options to calculate and use sample size
+    final BitmapFactory.Options bmpOptions = new BitmapFactory.Options();
+
+    //first decode image dimensions only - not the image bitmap itself
+    bmpOptions.inJustDecodeBounds = true;
+    BitmapFactory.decodeFile( imagePath, bmpOptions );
+
+    //image width and height before sampling
+    int currHeight = bmpOptions.outHeight;
+    int currWidth = bmpOptions.outWidth;
+
+    //variable to store new sample size
+    int sampleSize = 1;
+
+    //calculate the sample size if the existing size is larger than target size
+    if ( currHeight > targetHeight || currWidth > targetWidth )
+    {
+      //use either width or height
+      if ( currWidth > currHeight )
+      {
+        sampleSize = Math.round( (float) currHeight / (float) targetHeight );
+      }
+      else
+      {
+        sampleSize = Math.round( (float) currWidth / (float) targetWidth );
+      }
+    }
+    //use the new sample size
+    bmpOptions.inSampleSize = sampleSize;
+
+    //now decode the bitmap using sample options
+    bmpOptions.inJustDecodeBounds = false;
+
+    //get the file as a bitmap
+    final Bitmap pic = BitmapFactory.decodeFile(imagePath, bmpOptions);
+
+    view.setImageBitmap( pic );
+    view.setBackgroundColor( background );
   }
 }
